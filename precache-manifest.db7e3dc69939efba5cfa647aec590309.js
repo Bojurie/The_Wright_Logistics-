e66fbc18317a9f93d1cdab0d66c780420cd64ac0
MacBookAir <macbookair@MacBookAirs-MacBook-Air.local>
2022-05-29 05:16:09 -0700

    Updates

---
+++ precache-manifest.db7e3dc69939efba5cfa647aec590309.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3f63462b40caaf941df",
+    "url": "/The_Wright_Logistics-/static/css/main.1987cc3c.chunk.css"
+  },
+  {
+    "revision": "a3f63462b40caaf941df",
+    "url": "/The_Wright_Logistics-/static/js/main.f1d76bd2.chunk.js"
+  },
+  {
+    "revision": "da18f18a375a3e8aaed1",
+    "url": "/The_Wright_Logistics-/static/js/runtime~main.8dce26ad.js"
+  },
+  {
+    "revision": "94b2467a4df23c512665",
+    "url": "/The_Wright_Logistics-/static/css/2.c38bfd72.chunk.css"
+  },
+  {
+    "revision": "94b2467a4df23c512665",
+    "url": "/The_Wright_Logistics-/static/js/2.5e428d50.chunk.js"
+  },
+  {
+    "revision": "554e98201028f129431ba945a4ab0882",
+    "url": "/The_Wright_Logistics-/static/media/Logo-2.554e9820.png"
+  },
+  {
+    "revision": "385cb915606345379048840a8a38e4d9",
+    "url": "/The_Wright_Logistics-/static/media/image 7.385cb915.png"
+  },
+  {
+    "revision": "680f65d1b72949f2e2b990b1e2fa905b",
+    "url": "/The_Wright_Logistics-/static/media/image 8.680f65d1.png"
+  },
+  {
+    "revision": "4eeac519806991653a50b85a54e56fa2",
+    "url": "/The_Wright_Logistics-/static/media/Trucks.4eeac519.png"
+  },
+  {
+    "revision": "ab4a5a15da8bb905f6dcec90944b938e",
+    "url": "/The_Wright_Logistics-/static/media/IMG_3015.ab4a5a15.JPG"
+  },
+  {
+    "revision": "36948c319150058804e98c2003958ba3",
+    "url": "/The_Wright_Logistics-/static/media/Group 14.36948c31.png"
+  },
+  {
+    "revision": "edb65816bd138dab9e33cb25c1949a8b",
+    "url": "/The_Wright_Logistics-/static/media/smallmidsec.edb65816.png"
+  },
+  {
+    "revision": "e4b8df1eaf672308ec6cfc4c14cdac71",
+    "url": "/The_Wright_Logistics-/static/media/bg.e4b8df1e.jpg"
+  },
+  {
+    "revision": "743388d6ade092346af1d7f85f3e48f4",
+    "url": "/The_Wright_Logistics-/static/media/struck.743388d6.png"
+  },
+  {
+    "revision": "0ccc71ec1e159495485df6e6d0f9d389",
+    "url": "/The_Wright_Logistics-/static/media/contactperson.0ccc71ec.png"
+  },
+  {
+    "revision": "a5c382c5a4776ea0c7ba92207dbbdcbe",
+    "url": "/The_Wright_Logistics-/static/media/serviceimg.a5c382c5.png"
+  },
+  {
+    "revision": "19664d0c56fbfdae9967617779fb80a1",
+    "url": "/The_Wright_Logistics-/static/media/contactback.19664d0c.png"
+  },
+  {
+    "revision": "fa4e69e6b13a28069a5a866cdde2575a",
+    "url": "/The_Wright_Logistics-/static/media/midsecbg.fa4e69e6.png"
+  },
+  {
+    "revision": "a17ccfe57f7699a0d8265ca217a8f6e1",
+    "url": "/The_Wright_Logistics-/static/media/servicebg.a17ccfe5.png"
+  },
+  {
+    "revision": "05a500a61dc29cee6840483031dfb68c",
+    "url": "/The_Wright_Logistics-/static/media/footerbg.05a500a6.png"
+  },
+  {
+    "revision": "6e228f9e3d5288fccbe2883a37f6b0f1",
+    "url": "/The_Wright_Logistics-/index.html"
+  }
+];
